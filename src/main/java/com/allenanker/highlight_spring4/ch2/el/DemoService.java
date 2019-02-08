c01466916ad5158e40871a72174a0a43b050d799
TheAllenAnker <theallenanker@gmail.com>
2019-02-08 14:43:15 +0800

    Spring EL part of chapter 2.

---
+++ src/main/java/com/allenanker/highlight_spring4/ch2/el/DemoService.java
@@ -0,0 +1,18 @@
+package com.allenanker.highlight_spring4.ch2.el;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    @Value("Other class's property")
+    private String another;
+
+    public String getAnother() {
+        return another;
+    }
+
+    public void setAnother(String another) {
+        this.another = another;
+    }
+}
---
+++ src/main/java/com/allenanker/highlight_spring4/ch2/el/ResourceConfig.java
@@ -0,0 +1,9 @@
+package com.allenanker.highlight_spring4.ch2.el;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.allenanker.highlight_spring4.ch2.el")
+public class ResourceConfig {
+}
